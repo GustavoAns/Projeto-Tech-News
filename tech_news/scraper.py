0eb95675bc45d951958b6be4246ab24b80c29d18
GustavoAns <gugasato9@live.com>
2022-06-22 20:32:56 -0300

    Add req 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,4 +1,5 @@
 from parsel import Selector
+import re
 import time
 import requests
 
@@ -40,8 +41,38 @@ def scrape_next_page_link(html_content):
 
 
 # Requisito 4
+
+def scrape_quant_comments(html_content):
+    if html_content is None or html_content == '':
+        return 0
+    selector = Selector(html_content)
+    queryString = 'div.post-comments h5.title-block::text'
+    comments_text = selector.css(queryString).get()
+    if comments_text is None:
+        return 0
+    quant = re.sub('[^0-9]', '', comments_text)
+    comments_count = int(quant)
+    return comments_count
+
+
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    if html_content is None or html_content == '':
+        return None
+
+    selector = Selector(html_content)
+    comments_count = scrape_quant_comments(html_content)
+
+    sumQuery = 'div.entry-content'
+    return {
+        'url': selector.css('link[rel=canonical]::attr(href)').get(),
+        'title': selector.css('h1.entry-title::text').get(),
+        'timestamp': selector.css('li.meta-date::text').get(),
+        'writer': selector.css('a.url::text').get(),
+        'comments_count': comments_count,
+        'summary': selector.css(sumQuery).xpath('string(//p)').get(),
+        'tags': selector.css('a[rel=tag]::text').getall(),
+        'category': selector.css('a.category-style span.label::text').get()
+    }
 
 
 # Requisito 5
